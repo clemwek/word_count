b6e11cbfa1daddfd815969d7bb165478e31a162a
Clement Wekesa <clemwek@gmail.com>
2017-01-18 14:05:08 +0300

    complete solution

--- word_count.py
+++ word_count.py
@@ -9,12 +9,18 @@ def words(words):
 		words = words.split()
 		dic_out = {}
 		for word in words:
-			if word in dic_out:
-				dic_out[word] = dic_out[word]+1
+			if word.isdigit():
+				if int(word) in dic_out:
+					dic_out[int(word)] = dic_out[int(word)]+1
+				else:
+					dic_out[int(word)] = 1
 			else:
-				dic_out[word] = 1
+				if word in dic_out:
+					dic_out[word] = dic_out[word]+1
+				else:
+					dic_out[word] = 1
 		return dic_out
 	except Exception:
 		return 'invalid'
 
-# print(word_count("hello there we hello we are people from planet earth!"))
+print(words("hello there we hello we are people from planet earth 1 2 2 3"))
